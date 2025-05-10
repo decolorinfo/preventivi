bbda83eea546bf3dd0887b955d6c1903390060b1
Volodymyr Kashayev <166609673+decolorinfo@users.noreply.github.com>
2025-05-10 20:46:03 +0200

    Translate to Italian

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let rooms = [];
+let templates = ["Modello 1", "Modello 2"];
 
 function addRoom() {
     let roomIndex = rooms.length;
@@ -7,14 +8,14 @@ function addRoom() {
     let roomDiv = document.createElement("div");
     roomDiv.classList.add("room");
     roomDiv.innerHTML = `
-        <h4>Кімната ${roomIndex + 1}</h4>
-        <label>Назва: <input type="text" onchange="updateRoom(${roomIndex}, 'name', this.value)"></label><br>
-        <label>Довжина (м): <input type="number" onchange="updateRoom(${roomIndex}, 'length', this.value)"></label><br>
-        <label>Ширина (м): <input type="number" onchange="updateRoom(${roomIndex}, 'width', this.value)"></label><br>
-        <label>Висота (м): <input type="number" onchange="updateRoom(${roomIndex}, 'height', this.value)"></label><br>
-        <p>Площа стелі: <span id="ceilingArea${roomIndex}">0</span> м²</p>
-        <p>Площа стін: <span id="wallArea${roomIndex}">0</span> м²</p>
-        <label>Загальна площа кімнати: <input type="number" id="totalRoomArea${roomIndex}" onchange="adjustTotalRoomArea(${roomIndex}, this.value)"></label>
+        <h4>Stanza ${roomIndex + 1}</h4>
+        <label>Nome: <input type="text" onchange="updateRoom(${roomIndex}, 'name', this.value)"></label><br>
+        <label>Lunghezza (m): <input type="number" onchange="updateRoom(${roomIndex}, 'length', this.value)"></label><br>
+        <label>Larghezza (m): <input type="number" onchange="updateRoom(${roomIndex}, 'width', this.value)"></label><br>
+        <label>Altezza (m): <input type="number" onchange="updateRoom(${roomIndex}, 'height', this.value)"></label><br>
+        <p>Area soffitto: <span id="ceilingArea${roomIndex}">0</span> m²</p>
+        <p>Area pareti: <span id="wallArea${roomIndex}">0</span> m²</p>
+        <label>Area totale stanza: <input type="number" id="totalRoomArea${roomIndex}" onchange="adjustTotalRoomArea(${roomIndex}, this.value)"></label>
     `;
     document.getElementById("rooms").appendChild(roomDiv);
 }
@@ -47,13 +48,26 @@ function calculateTotal() {
     document.getElementById("finalCost").value = totalArea * pricePerMeter;
 }
 
+function addTemplate() {
+    let newTemplate = prompt("Inserisci il nome del nuovo modello:");
+    if (newTemplate) {
+        templates.push(newTemplate);
+        let select = document.getElementById("templateSelect");
+        let option = document.createElement("option");
+        option.value = newTemplate;
+        option.textContent = newTemplate;
+        select.appendChild(option);
+    }
+}
+
 function sendEmail() {
     let email = document.getElementById("email").value;
     let clientName = document.getElementById("clientName").value;
     let totalArea = document.getElementById("totalArea").value;
     let finalCost = document.getElementById("finalCost").value;
-    let subject = "Розрахунок вартості ремонту";
-    let body = `Клієнт: ${clientName}\nЗагальна площа (стелі + стін): ${totalArea} м²\nОстаточна сума: ${finalCost} грн`;
+    let template = document.getElementById("templateSelect").value;
+    let subject = "Calcolo del costo della ristrutturazione";
+    let body = `Cliente: ${clientName}\nModello: ${template}\nArea totale: ${totalArea} m²\nCosto finale: ${finalCost} €`;
 
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 }
